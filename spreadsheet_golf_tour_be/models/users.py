4b3e187de06ba4d61fbda343ead33043ab0ef846
vktr-r2 <122321007+vktr-r2@users.noreply.github.com>
2024-03-03 21:22:23 -0500

    BUILD - Add Users model and seed data

--- spreadsheet_golf_tour_be/models/users.py
+++ spreadsheet_golf_tour_be/models/users.py
@@ -1,7 +1,20 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from database import db
+from datetime import datetime
+from sqlalchemy import Column, DateTime, String
+from sqlalchemy_utils import UUIDType
+import uuid
 
 
 class Users(db.Model):
-    pass
+    __tablename__ = "users"
+
+    uuid = Column(UUIDType(binary=False), primary_key=True, default=uuid.uuid4)
+    email = Column(String(40), collation="utf8mb4_unicode_ci", nullable=False)
+    username = Column(String(16), collation="utf8mb4_unicode_ci", nullable=False)
+    password = Column(String(255), collation="utf8mb4_unicode_ci", nullable=False)
+    f_name = Column(String(16), collation="utf8mb4_unicode_ci", nullable=False)
+    l_name = Column(String(16), collation="utf8mb4_unicode_ci", nullable=False)
+    created_at = Column(DateTime(), nullable=False, default=datetime.utcnow)
+    updated_at = Column(
+        DateTime(), nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
