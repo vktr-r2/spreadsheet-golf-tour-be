bfffe783504001684d97b4eda7ba16ed38d5a375
vktr-r2 <122321007+vktr-r2@users.noreply.github.com>
2024-03-11 08:23:12 -0400

    BUILD - separate api request and endpoint setup to DRY

--- spreadsheet_golf_tour_be/services/rapid_api_wrapper.py
+++ spreadsheet_golf_tour_be/services/rapid_api_wrapper.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import requests
+from requests.exceptions import HTTPError, ConnectionError, Timeout, RequestException
 import os
 
 
@@ -14,10 +15,26 @@ class RapidApiWrapperService:
         }
 
     def call_schedule_endpoint(self, org_id):
-        url = f"{self.base_url}schedule"
         current_year = str(datetime.now().year)
-        querystring = {"orgID": org_id, "year": current_year}
+        query_string = {"orgID": org_id, "year": current_year}
+        return self.make_request("schedule", params=query_string)
 
-        response = requests.get(url, headers=self.headers, params=querystring)
+    def make_request(self, url_path, params=None):
+        full_url = f"{self.base_url}{url_path}"
 
-        return response.json()
+        try:
+            response = requests.get(full_url, headers=self.headers, params=params)
+            response.raise_for_status()
+            return response.json()
+
+        except HTTPError as http_err:
+            print(f"HTTP error occurred: {http_err}")
+        except ConnectionError as conn_err:
+            print(f"Connection error occurred: {conn_err}")
+        except Timeout:
+            print("The request timed out")
+        except RequestException as err:
+            print(f"An error occurred: {err}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+        return None
